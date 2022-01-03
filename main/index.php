ba3a5a7e70f5f9a7351f3e9a941b537683be4e68
unknown <rizkysentro3@gmail.com>
2022-01-03 09:01:11 +0700

    pembaruan

--- main/index.php
+++ main/index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE html> 
 <html lang="en">
 
 <head>
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Kecamatan Sumobito</title>
+    <title>Kecamatan Sumobitoo</title>
     <link rel="icon" type="image/x-icon" href="img/jombang.png" />
     <!-- Bootstrap icons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
